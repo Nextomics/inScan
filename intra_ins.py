c23cc990642ae202a8d0637c0b53b0b1cc00c572
Archieyoung <750826700@qq.com>
2018-03-20 21:30:07 +0800

    init

--- intra_ins.py
+++ intra_ins.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+#author: archieyoung<yangqi2@grandomics.com>
+"""
+find insertions from cigar
+"""
 
+import cigar_parse
+from inter_ins import insertion
+
+class intra_ins(object):
+    def __init__(self,sam_record):
