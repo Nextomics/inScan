028c09d25c508b1b1f89e583f11f4016ce94218c
Archieyoung <750826700@qq.com>
2018-04-17 21:13:27 +0800

    new fragment combination stategy

--- inter_ins.py
+++ inter_ins.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-#author archieyoung<yangqi2@grandomics.com>
-
 """
+author: archieyoung<yangqi2@grandomics.com>
+
 Find insertion from evidence of read split mapping.
 Duplication is a special case of insertion, so I treat it as insertion here
 """
-
-from itertools import combinations
 
 
 class insertion(object):
@@ -23,85 +20,91 @@ class inter_ins(object):
         self.min_len = min_len
         self.insertions = self._get_insert()
 
+    def _has_ins(self,fr1,fr2):
+        if fr1.strand != fr2.strand or fr1.ref != fr2.ref:
+            return 0
+        #then fr1 and fr2 are in the same ref and hava same strand
+        if fr1.strand == "+":
+            #fr_relativa_dist = fragment dist on reads - fragment dist on ref
+            fr_relativa_dist = ((fr2.query_start-fr1.query_end)-
+                    (fr2.ref_start-fr1.ref_end))
+        else:
+            fr_relativa_dist = ((fr2.query_start-fr1.query_end)-
+                    (fr1.ref_start-fr2.ref_end))
+        if fr_relativa_dist > 0:
+            #has insertion
+            return fr_relativa_dist
+        else:
+            return 0
+
+    def _calculate_ins(self,fr1,fr2,fr_relativa_dist):
+        if fr1.strand == "+":
+            #define ins_len and asign 0 to it
+            ins_len = 0
+            #insertion
+            #case1 and case2
+            if fr2.ref_start >= fr1.ref_end:
+                ins_len = fr2.query_start - fr1.query_end
+                ins_ref_start = fr1.ref_end
+                ins_ref_end = fr2.ref_start
+            #case3
+            if (fr2.ref_start < fr1.ref_end and
+                    fr2.ref_end >= fr1.ref_end):
+                ins_len = fr_relativa_dist
+                ins_ref_start = fr1.ref_end
+                ins_ref_end = fr1.ref_end
+            #case4
+            if (fr2.ref_start < fr1.ref_end and
+                    fr2.ref_end < fr1.ref_end):
+                ins_len = fr2.query_end - fr1.query_end
+                ins_ref_start = fr1.ref_end
+                ins_ref_end = fr1.ref_end
+        else:
+            #strand are "-"
+            #define ins_len and asign 0 to it
+            ins_len = 0
+            #insertion
+            #case1 or case2
+            if fr1.ref_start >= fr2.ref_end:
+                ins_len = fr2.query_start - fr1.query_end
+                ins_ref_start = fr2.ref_end
+                ins_ref_end = fr1.ref_start
+            #case3
+            if (fr1.ref_start < fr2.ref_end and
+                    fr1.ref_end >= fr2.ref_end):
+                ins_len = fr_relativa_dist
+                ins_ref_start = fr2.ref_end
+                ins_ref_end = fr2.ref_end
+            #case4
+            if (fr1.ref_start < fr2.ref_end and
+                    fr1.ref_end < fr2.ref_end):
+                ins_len = fr2.query_end - fr1.query_end
+                ins_ref_start = fr2.ref_end
+                ins_ref_end = fr2.ref_end
+
+        _ins = insertion(fr1.query_name,fr1.ref,ins_ref_start,
+                ins_ref_end,ins_len)
+
+        return _ins
+
+
     def _get_insert(self):
         if len(self.read_fragments) <= 1:
             return 0
 
         _insertions = []
-        #A,B
-        read_fragments_combine = combinations(self.read_fragments,2)
 
-        #fix bugs that the same insertion is counted more than once
-        ins_pos_left = []
+        #a new read fragment combination strategy, fix bug of false positive
+        #insertion discovery
+        while self.read_fragments:
+            for fr in self.read_fragments[1:]:
+                has_ins = self._has_ins(self.read_fragments[0],fr)
+                if has_ins:
+                    _ins = self._calculate_ins(self.read_fragments[0],
+                            fr,has_ins)
+                    if _ins.length >= self.min_len:
+                        _insertions.append(_ins)
+                        break
+            self.read_fragments.pop(0)
 
-        for fr in read_fragments_combine:
-            if fr[0].strand != fr[1].strand or fr[0].ref != fr[1].ref:
-                continue
-
-            if fr[0].strand == "+":
-                #imply that fr[1].strand == "+"
-                #fr_relativa_dist = fragment dist on reads - fragment dist on ref
-                fr_relativa_dist = ((fr[1].query_start-fr[0].query_end)-
-                        (fr[1].ref_start-fr[0].ref_end))
-                if fr_relativa_dist > 0:
-                    #define ins_len and asign 0 to it
-                    ins_len = 0
-                    #insertion
-                    #case1 and case2
-                    if fr[1].ref_start >= fr[0].ref_end:
-                        ins_len = fr[1].query_start - fr[0].query_end
-                        ins_ref_start = fr[0].ref_end
-                        ins_ref_end = fr[1].ref_start
-                    #case3
-                    if (fr[1].ref_start < fr[0].ref_end and
-                            fr[1].ref_end >= fr[0].ref_end):
-                        ins_len = fr_relativa_dist
-                        ins_ref_start = fr[0].ref_end
-                        ins_ref_end = fr[0].ref_end
-                    #case4
-                    if (fr[1].ref_start < fr[0].ref_end and
-                            fr[1].ref_end < fr[0].ref_end):
-                        ins_len = fr[1].query_end - fr[0].query_end
-                        ins_ref_start = fr[0].ref_end
-                        ins_ref_end = fr[0].ref_end
-                else:
-                    continue
-            else:
-                #strand are "-"
-                #fr_relativa_dist = fragment dist on reads - fragment dist on ref
-                fr_relativa_dist = ((fr[1].query_start-fr[0].query_end)-
-                        (fr[0].ref_start-fr[1].ref_end))
-                if fr_relativa_dist > 0:
-                    #define ins_len and asign 0 to it
-                    ins_len = 0
-                    #insertion
-                    #case1 or case2
-                    if fr[0].ref_start >= fr[1].ref_end:
-                        ins_len = fr[1].query_start - fr[0].query_end
-                        ins_ref_start = fr[1].ref_end
-                        ins_ref_end = fr[0].ref_start
-                    #case3
-                    if (fr[0].ref_start < fr[1].ref_end and
-                            fr[0].ref_end >= fr[1].ref_end):
-                        ins_len = fr_relativa_dist
-                        ins_ref_start = fr[1].ref_end
-                        ins_ref_end = fr[1].ref_end
-                    #case4
-                    if (fr[0].ref_start < fr[1].ref_end and
-                            fr[0].ref_end < fr[1].ref_end):
-                        ins_len = fr[1].query_end - fr[0].query_end
-                        ins_ref_start = fr[1].ref_end
-                        ins_ref_end = fr[1].ref_end
-                else:
-                    continue
-
-            _ins = insertion(fr[0].query_name,fr[0].ref,ins_ref_start,
-                    ins_ref_end,ins_len)
-
-            if _ins.length >= self.min_len:
-                if _ins.ref_start in ins_pos_left:
-                    continue
-                else:
-                    ins_pos_left.append(_ins.ref_start)
-                    _insertions.append(_ins)
         return _insertions
